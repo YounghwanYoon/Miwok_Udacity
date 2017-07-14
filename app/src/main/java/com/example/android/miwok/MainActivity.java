fbefcfa23e30b64b0d15acb4d5723f3d02127ec2
Ray Yoon <ray.yoon87@gmail.com>
2017-07-13 19:43:18 -0700

    Commit 3

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -18,24 +18,54 @@ package com.example.android.miwok;
 import android.app.LauncherActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        TextView numberView = (TextView) findViewById(R.id.family);
+        numberView.setOnClickListener(numberListener);
     }
 
+    /**
+     * Once Number column is clicked, then NumbersActivity.class/ its screen will be opened.
+     * @param view
+     */
     public void openNumbersList(View view){
         //TODO: Write your code here!
         Intent numberIntent = new Intent(this, NumbersActivity.class);
         startActivity(numberIntent);
     }
+
+    public void openColorsList(View view){
+        //TODO: Write your code here!
+        Intent colorIntent = new Intent(this, ColorsActivity.class);
+        startActivity(colorIntent);
+    }
+
+    public void openFamilyList(View view){
+        //TODO: Write your code here!
+        Intent familyIntent = new Intent(this, FamilyActivity.class);
+        startActivity(familyIntent);
+    }
+
+    private View.OnClickListener numberListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // do something when the button is clicked
+            openFamilyList(v);
+        }
+    };
+
+
 
 }
