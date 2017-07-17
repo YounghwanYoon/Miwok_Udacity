3af172c137ff21f323ecd3ea0c57754ae3fdfdfc
RayYoon <ray.yoon87@gmail.com>
2017-07-16 23:09:53 -0700

    ArrayList to store and make new TextView in NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import static android.os.Build.VERSION_CODES.M;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -27,19 +31,30 @@ public class NumbersActivity extends AppCompatActivity {
             k++;
         }
 
-
-
-
         //Create an array of numbers in Miwok
         String[] wordsMiwok =
                 new String[]{"lutti", "ottiko", "tolookosu", "oyyisa", "massoka", "temmokka", "kenekaku", "kawinta", "wo'e", "na'aacha"};
-        k= 0;
+        //Create Parent LinearLayout View
+        LinearLayout numberLinearLayout_View = (LinearLayout)findViewById(R.id.rootView);
+        TextView numberTextView = new TextView(this);
+        numberTextView.setText(words.get(0));
 
-        while(k < wordsEnglish.length){
-            Log.v("NumberActivity.java", "WordsEnglish at index "+k +": " + words.get(k));
-            Log.v("NumberActivity.java", "WordsMiwok at index " +k +": " + wordsMiwok[k]);
-            k++;
+        TextView numberTextViewTwo = new TextView(this);
+        numberTextViewTwo.setText(words.get(1));
+        //Add TextView as a child view to parents view, numberLinearLayout
+        numberLinearLayout_View.addView(numberTextViewTwo);
+
+
+        ArrayList<TextView> numberTextViewList = new ArrayList<TextView>();
+        int i = 0;
+        while(i < 9){
+            numberTextViewList.add(new TextView(this).setText(words.get(i)););
+
+            numberLinearLayout_View.addView(numberTextViewList.get(i));
+            i++
         }
+
+
 
     }
 
