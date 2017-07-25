5cb6fd1618063f3472a64befe0c9fd721f8b4546
Ray Yoon <ray.yoon87@gmail.com>
2017-07-24 19:42:16 -0700

    Commit 9
    Created Own Class called Word.java and implied it to store Miwok and English Translation Words.

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+        //ArrayList to store words
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        //Create an array of numbers in English and Miwok
+        String[] wordsEnglish =
+                new String[]{"Where are you going?", "What is your name?","My name is...","How are you feeling?","I'm feeling good.","Are you coming?","Yes,I'm coming","I'm coming","Let's go.","Come here."};
+        String[] wordsMiwok =
+                new String[]{"minto wuksus", "tinnә oyaase'nә", "oyaaset...", "michәksәs?", "kuchi achit", "әәnәs'aa?", "hәә’ әәnәm", "әәnәm", "yoowutis", "әnni'nem"};
+
+        int index = 0;
+        while(index < wordsEnglish.length)
+        {
+            words.add(new Word(wordsEnglish[index], wordsMiwok[index]));
+            index++;
+        }
+
+        WordAdapter itemsAdapter = new WordAdapter (this, words);
+
+        // Get a reference to the ListView, and attach the adapter to the listVew
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
